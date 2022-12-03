3856636e4a542426b896542a0191f7ecd8d81111
tdcau-py <borisov.p.92@yandex.ru>
2022-12-03 15:13:18 +0300

    Added function get_user_info

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ class VKUserAuth:
     vk = vk_api.VkApi(token=TOKEN)
 
 
+class VKUser:
+    """Собирает данные о пользователе"""
+    METHOD = 'users.get'
+
+    def __init__(self, id):
+        self.id = id
+
+    def get_myself_user_info(self, vk, user_id: int):
+        """Информация о пользователе"""
+        values = {'user_ids': user_id, 'fields': 'bdate, sex, city, relation'}
+        return vk.method(self.METHOD, values=values)
+
+
 def write_msg(vk, user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7), })
 
@@ -22,18 +35,21 @@ if __name__ == '__main__':
     vk_session = VKUserAuth()
     longpoll = VkLongPoll(vk_session.vk)
 
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
+    info = my_info(vk_session.vk, '749333920')
+    print(info)
 
-            if event.to_me:
-                request = event.text
-
-                if request == "привет":
-                    write_msg(vk_session.vk, event.user_id, f"Хай, {event.user_id}")
-                elif request == "пока":
-                    write_msg(vk_session.vk, event.user_id, "Пока((")
-                elif request == 'поиск':
-                    ...
-                else:
-                    write_msg(vk_session.vk, event.user_id, "Не поняла вашего ответа...")
+    # for event in longpoll.listen():
+    #     if event.type == VkEventType.MESSAGE_NEW:
+    #
+    #         if event.to_me:
+    #             request = event.text
+    #
+    #             if request == "привет":
+    #                 write_msg(vk_session.vk, event.user_id, f"Хай, {event.user_id}")
+    #             elif request == "пока":
+    #                 write_msg(vk_session.vk, event.user_id, "Пока((")
+    #             elif request == 'поиск':
+    #                 ...
+    #             else:
+    #                 write_msg(vk_session.vk, event.user_id, "Не поняла вашего ответа...")
                 
