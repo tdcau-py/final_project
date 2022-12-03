43d2e0e71b7de6949888470a5ede1c7ee049a589
tdcau-py <borisov.p.92@yandex.ru>
2022-12-03 13:36:40 +0300

    Modified to OOP

---
+++ db_manage.py
@@ -0,0 +1,54 @@
+import os
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
+Base = declarative_base()
+
+
+class City(Base):
+    __tablename__ = 'city'
+
+    id = Column(Integer, primary_key=True)
+    name_city = Column(String(length=40), nullable=False)
+
+
+class People(Base):
+    __tablename__ = 'people'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(length=40), nullable=False)
+    last_name = Column(String(length=40), nullable=False)
+    age = Column(Integer, nullable=False)
+    id_city = Column(Integer, ForeignKey('city.id'), nullable=False)
+    user_id = Column(Integer, nullable=False, unique=True)
+
+    cities = relationship(City, backref='people')
+
+    def __str__(self):
+        return f'User_VK: {self.first_name} {self.last_name}, user_id: {self.user_id}'
+
+
+def create_table(engine):
+    """Создает таблицы"""
+    ...
+
+
+def create_db():
+    """Создает базу данных"""
+    LOGIN = os.getenv('DB_LOGIN')
+    PASSWORD = os.getenv('DB_PASSWORD')
+    HOST = os.getenv('DB_HOST')
+    PORT = os.getenv('DB_PORT')
+    DATABASE = 'vk_search_result'
+
+    DSN = f'postgresql://{LOGIN}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
+    engine = sqlalchemy.create_engine(DSN)
+
+    create_table(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.close()
--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 from random import randrange
-from access_token import GROUP_ACCESS_TOKEN
-
+import  os
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token = input('Token: ')
 
-vk = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vk)
+class VKUserAuth:
+    """Авторизация пользователя"""
+    TOKEN = os.getenv('VK_TOKEN')
+
+    if not TOKEN:
+        TOKEN = input('Token: ')
+
+    vk = vk_api.VkApi(token=TOKEN)
 
 
-def write_msg(user_id, message):
+def write_msg(vk, user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7), })
 
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
+if __name__ == '__main__':
+    vk_session = VKUserAuth()
+    longpoll = VkLongPoll(vk_session.vk)
 
-        if event.to_me:
-            request = event.text
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
 
-            if request == "привет":
-                write_msg(event.user_id, f"Хай, {event.user_id}")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
-            else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+            if event.to_me:
+                request = event.text
+
+                if request == "привет":
+                    write_msg(vk_session.vk, event.user_id, f"Хай, {event.user_id}")
+                elif request == "пока":
+                    write_msg(vk_session.vk, event.user_id, "Пока((")
+                elif request == 'поиск':
+                    ...
+                else:
+                    write_msg(vk_session.vk, event.user_id, "Не поняла вашего ответа...")
                 
